3c68d89d01b690e12da36c6f72e7eeaaa6611975
ldnicolasmay <ldnicolasmay@gmail.com>
2023-09-13 22:13:17 -0400

    chore: clean up commented-out code

--- src/main/java/com/streamwork/ch04/job/FatTransactionEvent.java
+++ src/main/java/com/streamwork/ch04/job/FatTransactionEvent.java
@@ -9,13 +9,6 @@ import java.util.Date;
  * A simple transaction event used in the fraud detection job.
  */
 public class FatTransactionEvent extends TransactionEvent {
-  //public final static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
-
-  //public final String transactionId;
-  //public final float amount;
-  //public final Date transactionTime;
-  //public final long merchandiseId;
-  //public final long userAccount;
   public final String foo;
 
   public FatTransactionEvent(String transactionId, float amount, Date transactionTime, long merchandiseId, long userAccount, String foo) {
